a9acc9168a28caa9aa04b631e542c38bfac6896c
ksy <sybz0748@gmail.com>
2024-08-27 11:11:47 +0900

    fix: 시큐리티 일반유저/어드민 충돌 해결

--- src/main/java/iGuard/Server/Service/user/MemberUserDetailsService.java
+++ src/main/java/iGuard/Server/Service/user/MemberUserDetailsService.java
@@ -3,26 +3,28 @@ package iGuard.Server.Service.user;
 import iGuard.Server.Entity.User;
 import iGuard.Server.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
-public class SecurityUserService implements UserDetailsService {
+public class MemberUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.getById(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + username));
+
         return new org.springframework.security.core.userdetails.User(
                 user.getId(),
                 user.getPassword(),
-                new ArrayList<>());
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_MEMBER")));
     }
 }
