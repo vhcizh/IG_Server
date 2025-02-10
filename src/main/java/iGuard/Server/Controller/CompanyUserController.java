d07375f7b5357adf9f45c02f8953d77a9fcbdd63
ksy <sybz0748@gmail.com>
2025-02-10 23:45:49 +0900

    fix: 어드민 수정 및 탈퇴 오류 해결 - 어드민 비밀번호 수정 후 message가 나타나지 않는 문제 - 탈퇴 후 로그인 상태가 유지되는 문제

--- src/main/java/iGuard/Server/Controller/CompanyUserController.java
+++ src/main/java/iGuard/Server/Controller/CompanyUserController.java
@@ -3,12 +3,18 @@ package iGuard.Server.Controller;
 import iGuard.Server.Entity.CompanyUser;
 import iGuard.Server.Service.admin.CompanyUserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.Optional;
 
 @Slf4j
@@ -36,7 +42,7 @@ public class CompanyUserController {
     public String updatePassword(@RequestParam int companyUserId,
                                  @RequestParam String currentPassword,
                                  @RequestParam String newPassword,
-                                 Model model) {
+                                 RedirectAttributes redirectAttributes) {
         Optional<CompanyUser> companyUserOpt = companyUserService.getCompanyUserById(companyUserId);
 
         if (companyUserOpt.isPresent()) {
@@ -46,20 +52,26 @@ public class CompanyUserController {
             if (companyUserService.checkPassword(companyUser, currentPassword)) {
                 // Update the password
                 companyUserService.updatePassword(companyUser, newPassword);
-                model.addAttribute("message", "비밀번호가 성공적으로 변경되었습니다.");
+                redirectAttributes.addFlashAttribute("message", "비밀번호가 성공적으로 변경되었습니다.");
             } else {
-                model.addAttribute("message", "현재 비밀번호가 일치하지 않습니다.");
+                redirectAttributes.addFlashAttribute("message", "현재 비밀번호가 일치하지 않습니다.");
             }
         } else {
-            model.addAttribute("message", "사용자를 찾을 수 없습니다.");
+            redirectAttributes.addFlashAttribute("message", "사용자를 찾을 수 없습니다.");
         }
 
         return "redirect:/admin/mypage";
     }
 
     @PostMapping("/mypage/delete")
-    public String deleteCompanyUser(@RequestParam int companyUserId) {
+    public String deleteCompanyUser(@RequestParam int companyUserId, HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes) {
         companyUserService.deleteCompanyUser(companyUserId);
-        return "redirect:/common/home";
+        // 로그아웃 처리
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        redirectAttributes.addFlashAttribute("message", "회원탈퇴 되었습니다.");
+        return "redirect:/admin/login";
     }
 }
