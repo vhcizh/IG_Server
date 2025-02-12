199f485951dd7fe79340a86d7aa20e04a63adf0b
ksy <sybz0748@gmail.com>
2025-02-12 14:02:22 +0900

    fix: 403 에러 발생시 빈화면이 뜨는 문제
    - 조건 구체화
    - 해당 조건 외에는 403에러 정상적으로 발생하도록 구현

--- src/main/java/iGuard/Server/Component/CustomAccessDeniedHandler.java
+++ src/main/java/iGuard/Server/Component/CustomAccessDeniedHandler.java
@@ -3,17 +3,21 @@ package iGuard.Server.Component;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.csrf.CsrfException;
 
 import java.io.IOException;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        // 어드민 -> 유저
-        if (request.isUserInRole("ADMIN")) {
+        // 어드민이 /common/mypage에 접근 시 /admin/mypage로 리다이렉트
+        if (request.isUserInRole("ADMIN") && request.getRequestURI().startsWith("/common/mypage")) {
             response.sendRedirect("/admin/mypage");
         }
+        // 기본 403 에러 발생
+        response.sendError(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase());
     }
 }
