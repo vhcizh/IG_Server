92b3c4b0b5f4a639a83bfb41fd01c697e3ceffd2
vhcizh <sybz0748@gmail.com>
2025-03-21 15:17:05 +0900

    feat: 메인페이지 변경
    - 기존 home 삭제
    - 처음 접속했을 때 위치정보 저장

--- src/main/resources/static/common/js/header.js
+++ src/main/resources/static/common/js/header.js
@@ -1,20 +1,32 @@
+const latitude = getCookie("latitude");
+const longitude = getCookie("longitude");
+
 document.addEventListener("DOMContentLoaded", function() {
-    const latitude = getCookie("latitude");
-    const longitude = getCookie("longitude");
-
     if (!latitude || !longitude) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
+        getLocation()
+            .then(() => console.log("위치 정보 설정 완료"))
+            .catch(() => console.warn("위치 정보를 가져오지 못했습니다."));
 
-            setCookie("latitude", lat, 1); // 1일 동안 유지
-            setCookie("longitude", lng, 1);
-
-        }, function(error) {
-            console.error("위치 정보를 가져올 수 없습니다.", error);
-        });
     }
 });
+
+function getLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            setCookie("latitude", lat, 1); // 1일 동안 유지
+            setCookie("longitude", lon, 1);
+
+            resolve({lat, lon}); // 성공하면 좌표 반환
+
+        }, function (error) {
+            console.error("위치 정보를 가져올 수 없습니다.", error);
+            reject(error); // 실패 시 에러 반환
+        });
+    });
+}
 
 // 쿠키 설정 함수
 function setCookie(name, value, days) {
